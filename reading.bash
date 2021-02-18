102bdbd3622f6ec85c588e072a88df4bfb099e79
David Morrin <dm187@nyu.edu>
2021-02-18 16:47:02 -0500

    feat: log and POST readings, parse response with jq

--- reading.bash
+++ reading.bash
@@ -1,20 +1,65 @@
 #!/bin/bash
-# using DHTXXD, see http://abyz.me.uk/rpi/pigpio/code/DHTXXD.zip
-# on Raspberry Pi with sensor connected to GPIO 21
+# Temperature & humidity readings logged locally and on the web.
+# Intended to be run on boot (via systemd or similar) and run continuously.
 
+# web app returns JSON, requiring `jq` to parse JSON
+command -v jq &>/dev/null || {
+  echo "$(basename "$0") requires jq, aborting"
+  echo 'try "sudo apt-get update && sudo apt-get install jq"'
+  exit 1
+}
+
+# Using DHTXXD, see http://abyz.me.uk/rpi/pigpio/code/DHTXXD.zip
+# on Raspberry Pi with sensor connected to GPIO 21
 gpioPin=21
 
-# reading should have an error code, temperature in C, and humidity
-# output of DHTXXD is read into an array
-reading=("$(DHTXXD -g$gpioPin)")
+# number of seconds between readings
+interval=120
 
-# put reading into JSON
-data='{'
-if [[ ${reading[0]} != 0 ]]; then
-  data+='"error":'"${reading[0]}}"
-else
-  data+='"temperature":'"${reading[1]}}"
-fi
-# POST reading to URL
-url="https://script.google.com/YourScriptURL/exec"
-curl -L -H "Content-Type: application/json" -d '' "$url"
+# name of file to keep a local log in case of network issues
+logFile=dhtxxd.log
+errorLogFile=dhtxxd.error.log
+
+# calculate max size based on choice of interval
+maxLines=$((60*60*24*31/interval)) # ~ number of readings in a month
+maxLogSize=$((maxLines * 14)) # estimate 14 bytes per reading
+
+truncateLog() {
+  # get actual number of lines
+  read -r lines _ <<< "$(wc -l "$1")"
+  # delete the first half of the file
+  sed -i -n $((lines/2))',$p' "$1"
+}
+
+logToFile() {
+  echo "$(date +"%Y-%m-%dT%H:%M:%S") $2" >>"$1"
+  logSize=$(stat --printf="%s" "$1")
+  ((logSize > maxLogSize)) && truncateLog "$1"
+}
+
+postToWebApp() {
+  url="https://script.google.com/YourScriptURL/exec"
+  # POST reading to URL, follow redirects, set header to JSON
+  curl -L -H "Content-Type: application/json" -d "$1" "$url"
+}
+
+# reading should have an error code, temperature in Celcius, and % humidity
+# defeating stdbuf so output is immediate
+stdbuf --output=0 ./DHTXXD -g"$gpioPin" -i"$interval" \
+  | while read -r error temperature humidity; do
+
+    logToFile "$logFile" "$error $temperature $humidity"
+
+    if ((error != 0)); then
+      response="$(postToWebApp "{\"error\":$error}")"
+    else
+      response="$(postToWebApp "{\"temperature\":$temperature,\"humidity\":$humidity}")"
+    fi
+
+    # existence of error key means an error occured
+    error="$(jq -r .error <<<"$response")"
+    if [[ -n $error ]]; then
+      logToFile "$errorLogFile" "$error"
+    fi
+
+  done
